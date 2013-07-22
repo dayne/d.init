2dc28c1f9ba5c9fabec53d476bd7cadbce5717af
Dayne Broderson <broderson@gmail.com>
2013-07-22 06:25:46 +0000

    rejiggering to ask before going forward with blamo

--- ubuntu.sh
+++ ubuntu.sh
@@ -6,12 +6,18 @@
 # get my generic stuff available
 source lib/helpers.sh
 
-ubuntu_install "build-essential libreadline6-dev curl zlib1g-dev openssl libssl-dev bison"
-ubuntu_install "libreadline-dev git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev "
-ubuntu_install "sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool"
+ubuntu_install 'tmux curl wget feh vim'
 
-ubuntu_install 'tmux curl wget feh'
+agree "install build essentials and compiling stuff?"
+if [ $? -eq 0 ]; then
+	ubuntu_install "build-essential libreadline6-dev curl zlib1g-dev openssl libssl-dev bison"
+	ubuntu_install "libreadline-dev git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev "
+	ubuntu_install "sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool"
+fi
 
-./rbenv
-./ruby
-./tat
+agree "go ahead and go nuts with rbenv, ruby and tat?"
+if [ $? -eq 0 ]; then
+	./rbenv
+	./ruby
+	./tat
+fi
